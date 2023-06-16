9394ccc6ed5c9d0c4cd7acd0fe3ec48442dff06d
nicoalejo <nicoalejor@gmail.com>
2023-06-15 23:07:22 -0500

    *Added Final Screen

--- Assets/Scripts/PickNumberHandler.cs
+++ Assets/Scripts/PickNumberHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class PickNumberHandler : MonoBehaviour
 {
+    [SerializeField] private int finalNumber = 60;
     [SerializeField] private int[] numbersToPickArray = new int [12];
     [SerializeField] private GameObject btnNumberToPick;
     [SerializeField] private Transform panelNumbersToPick;
@@ -20,22 +21,27 @@ public class PickNumberHandler : MonoBehaviour
         }
 
         List<int> numbersAchieved = SaveManager.instance.GetCompletedNumbers();
-
-        for (int i = 0; i < numbersToPickArray.Length; i++)
+        //If all numbers are achieved, show the final screen
+        if (numbersAchieved.Count >= 12)
         {
-             GameObject currentNumberToPick = Instantiate(btnNumberToPick, panelNumbersToPick);
-             currentNumberToPick.GetComponentInChildren<TextMeshProUGUI>().text = numbersToPickArray[i].ToString();
-             var tempValue = i;         //Creates a temp value for i to send to the anonymous function
-             currentNumberToPick.GetComponent<Button>().onClick.AddListener(() => onNumberPicked?.Invoke(numbersToPickArray[tempValue])) ;
-             //Change color of already completed numbers
-             if (numbersAchieved != null && numbersAchieved.Contains(numbersToPickArray[i]))
-             {
-                 currentNumberToPick.GetComponent<Image>().color = new Color(209, 0, 20);
-             }
+            GameObject currentNumberToPick = Instantiate(btnNumberToPick, panelNumbersToPick);
+            currentNumberToPick.GetComponentInChildren<TextMeshProUGUI>().text = "Final";
+            currentNumberToPick.GetComponent<Button>().onClick.AddListener(() => onNumberPicked?.Invoke(finalNumber));       
         }
-        
-        
+        else
+        {
+            for (int i = 0; i < numbersToPickArray.Length; i++)
+            {
+                GameObject currentNumberToPick = Instantiate(btnNumberToPick, panelNumbersToPick);
+                currentNumberToPick.GetComponentInChildren<TextMeshProUGUI>().text = numbersToPickArray[i].ToString();
+                var tempValue = i;         //Creates a temp value for i to send to the anonymous function
+                currentNumberToPick.GetComponent<Button>().onClick.AddListener(() => onNumberPicked?.Invoke(numbersToPickArray[tempValue])) ;
+                //Change color of already completed numbers
+                if (numbersAchieved != null && numbersAchieved.Contains(numbersToPickArray[i]))
+                {
+                    currentNumberToPick.GetComponent<Image>().color = new Color(209, 0, 20);
+                }
+            }    
+        }
     }
-
-    
 }
